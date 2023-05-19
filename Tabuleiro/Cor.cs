8195e6be9184d1327fefa6655b4d9a899d40f86a
Arthur Minella <arthur.minella@gmail.com>
2023-05-19 17:34:15 -0300

    Começando a implementar o tabuleiro

---
+++ Tabuleiro/Cor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace tabuleiro
+{
+    enum Cor
+    {
+        Branca = 0,
+        Preta = 1,
+        Amarela = 2,
+        Vermelha = 3,
+        Verde = 4,
+        Laranja = 5,
+    }
+}
