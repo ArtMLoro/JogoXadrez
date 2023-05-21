2c4bfe0ee6f2fdc5093b65ed32b01dc37ca63f9b
Arthur Minella <arthur.minella@gmail.com>
2023-05-21 17:39:15 -0300

    Tratando erros com exceção personalizada

--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using tabuleiro;
+
 
 namespace tabuleiro
 {
@@ -20,10 +22,42 @@ namespace tabuleiro
             return Pecas[linha, coluna];
         }
 
+        public Peca Peca (Posicao posicao)
+        {
+            return Pecas[posicao.Linha, posicao.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha<0 || pos.Linha>=Linhas || pos.Coluna<0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;    
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
